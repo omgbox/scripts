9d5db26c5d71cad1eb010c05a073369b621c56cf
omgbox <alexander.alisani@icloud.com>
2015-03-08 20:42:15 +0100

    quick static ffmpeg setup

---
+++ install_ffmpeg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz
+
+sleep 1
+
+tar xvf ffmpeg.static.32bit.latest.tar.gz
+
+sleep 1
+
+cp ffmpeg /usr/bin/ffmpeg
+
+
+
