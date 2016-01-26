64af0098fad6f722f419d69ea1f6ba4a802a2f20
omgbox <omgbox@users.noreply.github.com>
2016-01-26 16:24:29 +0000

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz
-
+# wget -qO- https://raw.githubusercontent.com/omgbox/scripts/master/install_ffmpeg.sh | bash
 sleep 1
 
 tar xvf ffmpeg.static.32bit.latest.tar.gz
