1244c179ac3c91b979d314cfca83c7d9819a944d
Kandosan <omgbox@users.noreply.github.com>
2024-02-18 22:10:35 +0000

    Update caddyup.sh

--- caddyup.sh
+++ caddyup.sh
@@ -2,3 +2,25 @@
 #caddy reverse-proxy --from youtube.com www.youtube.com --to localhost:3000
 # use pm2 to start this bash script
 caddy run --config /etc/caddy/Caddyfile
+
+
+-------------------------------------------------------------
+
+MAKE FILE WITH NANO
+nano /etc/caddy/Caddyfile
+
+copy inside 
+--------------------------------------------
+domain.ovh {
+    reverse_proxy localhost:3000 
+    # tell Caddy to reverse proxy that port to your subdomain
+}
+www.rsd.ovh {
+  redir https://domain.ovh{uri} permanent
+}
+http://www.domain.ovh {
+  redir https://domain.ovh{uri} permanent
+}
+https://www.domain.ovh {
+  redir https://domain.ovh{uri} permanent
+}
